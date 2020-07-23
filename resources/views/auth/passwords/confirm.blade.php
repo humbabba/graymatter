2f1f9531b54bcffe972445521bb294fa3898f314
Charles Gray <charles.gray@komando.com>
2020-07-23 05:53:28 -0700

    Finishes authentication templates

--- resources/views/auth/passwords/confirm.blade.php
+++ resources/views/auth/passwords/confirm.blade.php
@@ -1,49 +1,38 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Confirm Password') }}</div>
+  <div class="centum">
+     <div class="cell x30 center">
+        <h1 class="pv15">{{ __('Confirm password') }}</h1>
+        @error('password')
+          <div class="alert error">
+            {{ $message }}
+          </div>
+        @enderror
+        <p>{{ __('Please confirm your password before continuing.') }}</p>
+        <form method="POST" action="{{ route('password.confirm') }}">
+          @csrf
+          <label for="email">{{ __('Password') }}</label>
+          <input id="password" type="password" class="@error('password') is-invalid @enderror" name="password" required autocomplete="current-password" autofocus>
+          <div class="centum">
 
-                <div class="card-body">
-                    {{ __('Please confirm your password before continuing.') }}
+          </div>
 
-                    <form method="POST" action="{{ route('password.confirm') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Confirm Password') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+          <div class="centum">
+             <div class="cell p0 x20">
+                <button type="submit" class="btn btn-primary">
+                {{ __('Go') }}
+                </button>
+              </div>
+              <div class="cell ph0 x80 align-right-d">
+                @if (Route::has('password.request'))
+                  <a href="{{ route('password.request') }}">
+                  {{ __('Forgot your password?') }}
+                </a>
+                @endif
+             </div>
+          </div>
+        </form>
+     </div>
+  </div>
 @endsection
--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
   <div class="centum">
-     <div class="cell x35 center">
+     <div class="cell x30 center">
         <h1 class="pv15">{{ __('Verify your email address') }}</h1>
         @if (session('resent'))
           <div class="alert success">
@@ -10,11 +10,10 @@
           </div>
         @endif
         <p>{{ __('Before proceeding, please check your email for a verification link.') }}</p>
-        <p>{{ __('If you did not receive the email') }},</p>
         <form method="POST" action="{{ route('verification.resend') }}">
           @csrf
           <button type="submit" class="btn btn-primary">
-            {{ __('click here to request another') }}
+            {{ __('Click here to request another link') }}
           </button>
         </form>
      </div>
