4f05ac49108f19b9a55f85fe33478ca0e194aa83
Charles Gray <charles.gray@komando.com>
2020-08-02 19:13:04 -0700

    Adds timestamp to suspended tool tip

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -89,7 +89,7 @@
             <span class="header-m">Username:</span>
             <span class="center-v">{{ $user->name }}</span>
             @if($user->isSuspended())
-              <i class="fas fa-ban" title="Suspended"></i>
+              <i class="fas fa-ban" title="Suspended till {{ $user->suspended_till }} UTC"></i>
             @else
               @if($user->email_verified_at)
                 <i class="fas fa-check" title="Verified"></i>
