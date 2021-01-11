b455697aa79ee095121c383576fc30ac0c15e383
Charles Gray <charles.gray@komando.com>
2021-01-11 14:02:17 -0700

    Adds delete modal setup

--- resources/js/script.js
+++ resources/js/script.js
@@ -6,17 +6,12 @@
 import { CentaModal } from './centa/components/modal.js';
 
 //User suspension
-const suspendUser = (userId,form,suspendedDays,suspendedMessage) => {
-    console.log('Suspend func stuff');
-    console.log(userId);
-    console.log(form);
-    console.log(suspendedDays);
-    console.log(suspendedMessage);
+const suspendUser = (form,suspendedDays,suspendedMessage) => {
     form.find('input[name="suspendedDays"]').val(suspendedDays);
     form.find('input[name="suspendedMessage"]').val(suspendedMessage);
     form.submit();
 };
-
+//User suspension modal
 $('.suspendUser').each((index,el) => {
     const button = $(el);
     const form = button.closest('form');
@@ -32,11 +27,37 @@ $('.suspendUser').each((index,el) => {
         const modal = new CentaModal({
             titleText: 'Suspend this user?',
             contentHtml: modalContent,
-            params: [userId,form],
+            params: [form],
             inputNames: ['suspendedDays','suspendedMessage'],
             cancelText: 'Cancel',
             confirmText: 'Suspend',
         },suspendUser);
+        modal.render();
+    });
+
+});
+
+//User deletion
+const deleteUser = form => {
+    form.submit();
+};
+//User deletion modal
+$('.deleteUser').each((index,el) => {
+    const button = $(el);
+    const form = button.closest('form');
+    const userName = form.find('input[name="suspendedUserName"]').val();
+    const userId = form.find('input[name="suspendUserId"]').val();
+    const modalContent = `<p>Do you really want to delete user <span class="username">${userName}</span> (ID: <span class="userId">${userId}</span>)?</p>`;
+    button.on('click',(e) => {
+        e.preventDefault();
+        const modal = new CentaModal({
+            titleText: 'Delete this user?',
+            contentHtml: modalContent,
+            params: [form],
+            inputNames: [],
+            cancelText: 'Cancel',
+            confirmText: 'Delete user',
+        },deleteUser);
         modal.render();
     });
 
@@ -47,10 +68,10 @@ const thing = url => alert('I got: ' + url);
 
 $('#testButton').on('click',() => {
     let modal = new CentaModal({
-        titleText:'<h1>Link destination</h1>',
-        contentHtml: '<label>Enter URL:</label><input type="text" name="url" />',
-        inputNames: ['url']
-    }
-    ,thing);
+            titleText:'<h1>Link destination</h1>',
+            contentHtml: '<label>Enter URL:</label><input type="text" name="url" />',
+            inputNames: ['url']
+        }
+        ,thing);
     modal.render();
 });
