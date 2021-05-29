a9f51014e938338a4172e406e1ff5a25f0fa7482
Charles Gray <charles.gray@komando.com>
2021-05-29 12:23:32 -0700

    Adds pronouns to users config

--- config/users.php
+++ config/users.php
@@ -16,4 +16,35 @@ return [
       'create' => env('USERS_CREATE', true),
     ],
 
+    /*
+    |--------------------------------------------------------------------------
+    | Pronouns
+    |--------------------------------------------------------------------------
+    |
+    | What pronouns do we allow?
+    |
+    */
+
+    'pronouns' => [
+      'masculine' => [
+        'subjective' => 'he',
+        'objective'  => 'him',
+        'possessive' => 'his'
+      ],
+      'feminine' => [
+        'subjective' => 'she',
+        'objective'  => 'her',
+        'possessive' => 'her'
+      ],
+      'neuter' => [
+        'subjective' => 'it',
+        'objective'  => 'it',
+        'possessive' => 'its'
+      ],
+      'plural' => [
+        'subjective' => 'they',
+        'objective'  => 'them',
+        'possessive' => 'their'
+      ],
+    ],
 ];
